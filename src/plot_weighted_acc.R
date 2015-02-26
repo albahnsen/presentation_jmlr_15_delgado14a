eb73d68b07edefb57dd957052ac707f194e48d48
AlBahnsen <al.bahnsen@gmail.com>
2015-02-26 12:45:56 +0100

    modify weighted plot, names as xlabels, no colors

--- src/plot_weighted_acc.R
+++ src/plot_weighted_acc.R
@@ -44,19 +44,25 @@ plot_weighted_acc <- function(selected_algos,colors,results_ranking,info_tables,
                "Weighted accuracy # features" = "ui"
   )
   
-  res$order[order(-res[,result_stat])] <- 1:length(a)
+  # Always order by um
+  res$order[order(-res[,"um"])] <- 1:length(a)
 
-  ggplot(res[order(-res[,result_stat]),], aes_string(x="order", y=result_stat) ) +  
-    geom_line(linetype="dashed") + geom_point(alpha=0.5, size=7, aes(color=Algorithm),shape=19) + scale_color_manual(values = colors) +
-    xlab("Algorithm") + ylab(names(accu)[match(result_stat, accu)]) + scale_x_discrete(breaks = 1:length(selected_algos), aes(limit=Algorithm)) +
-    geom_line(linetype="dashed", alpha=0.2, aes(x=order,y=um)) + geom_point(alpha=0.3, size=3, aes(x=order,y=um,color=Algorithm),shape=19) + 
-    theme(text = element_text(size=20)) + ggtitle("Accuracy and weighted accuracy of selected classifiers")
+  ggplot(res, aes(x=reorder(Algorithm,-um), y=um)) +  
+    geom_bar(width = 0, alpha=0, fill="black", stat="identity") +
+    theme(text = element_text(size=20),legend.position="none", axis.text.x = element_text(angle = 45, hjust = 1)) +
+    geom_line(linetype="dashed", alpha=0.2, aes(x=order,y=um)) + geom_point(alpha=0.3, size=3, aes(x=order,y=um),color="#FF0000FF",shape=19) +
+    geom_line(linetype="dashed", aes_string(x="order",y=result_stat)) + 
+    geom_point(alpha=0.5, size=7,  color="#FF0000FF",shape=19, aes_string(x="order",y=result_stat)) + 
+    ylim(min(res$um,res[,result_stat]),max(res$um,res[,result_stat])) +
+    xlab("Algorithm") + ylab(names(accu)[match(result_stat, accu)]) + 
+    ggtitle("Accuracy and weighted accuracy of selected classifiers")
   
+#   ggplot(res[order(-res[,result_stat]),], aes_string(x="order", y=result_stat) ) +  
+#     geom_line(linetype="dashed") + geom_point(alpha=0.5, size=7, aes(color=Algorithm),shape=19) + scale_color_manual(values = colors) +
+#     xlab("Algorithm") + ylab(names(accu)[match(result_stat, accu)]) + scale_x_discrete(breaks = 1:length(selected_algos), aes(limit=Algorithm)) +
+#     geom_line(linetype="dashed", alpha=0.2, aes(x=order,y=um)) + geom_point(alpha=0.3, size=3, aes(x=order,y=um,color=Algorithm),shape=19) + 
+#     theme(text = element_text(size=20)) + ggtitle("Accuracy and weighted accuracy of selected classifiers")
+#   
 }
 
-# plot_weighted_acc(selected_algos,colors,results_ranking,info_tables,"ui","Binary")
-
-
-
-
-
+# plot_weighted_acc(selected_algos,colors,results_ranking,info_tables,"ui","All")
